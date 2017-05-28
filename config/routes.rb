c0326b087bd880638fac6fcc8d315e3183479ba9
kalaksa <a.zachariasiewicz@gmail.com>
2017-05-28 19:30:32 +0200

    Add bootstrap to application

--- config/routes.rb
+++ config/routes.rb
@@ -6,3 +6,5 @@ Rails.application.routes.draw do
   resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+ # get  '/help',    to: 'static_pages#help'
