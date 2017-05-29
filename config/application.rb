8d9888c625a0af3b2f62683b35a217f5a9257476
kalaksa <a.zachariasiewicz@gmail.com>
2017-05-29 21:48:32 +0200

    Add config for action mailer

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module Cookbook
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    ActionMailer::Base.smtp_settings = {
+      :address  => 'smtp.gmail.com',
+      :domain => 'mail.google.com',
+      :port => 587,
+      :user_name => 'cookbookabgaro@gmail.com',
+      :password => 'abgaro1234',
+      :authentication => 'login',
+      :enable_starttls_auto => true
+    }
   end
 end
