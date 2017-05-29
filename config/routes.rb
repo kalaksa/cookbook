1a4dc87863529924bb38e5a42076da085acd6c29
kalaksa <a.zachariasiewicz@gmail.com>
2017-05-29 22:09:30 +0200

    Add devise and authentication related changes in views

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
   resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
  # get  '/help',    to: 'static_pages#help'
