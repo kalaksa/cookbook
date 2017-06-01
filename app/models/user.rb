2e21f1c8c54749d670c4ef4a523ab1e60e8ced06
kalaksa <a.zachariasiewicz@gmail.com>
2017-06-01 12:21:28 +0200

    Add username field to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
   has_many :recipes
   has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'recipes#index'
   resources :recipes do
     resources :comments
